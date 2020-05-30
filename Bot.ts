84357f4e52920f0b0e1f8d935cc504e519ee63a0
Toby <toby@tobymeehan.com>
2020-05-30 07:15:15 +0100

    Add configuration
    Added configuration to bot class

--- Bot.ts
+++ Bot.ts
@@ -1,10 +1,12 @@
 import { Client } from "discord.js";
+import Config from "./Configuration.json";
 
 /**
  * Represents the current bot.
  */
 class Bot {
     static client: Client;
+    static configuration = Config;
 }
 
 export = Bot;
