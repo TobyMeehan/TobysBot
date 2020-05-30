d2da8f1e59eb92f2fd5a288b76b754d938ac417e
Toby <toby@tobymeehan.com>
2020-05-30 09:00:50 +0100

    Add summon command
    Added command for summoning users

---
+++ Commands/SummonCommand.ts
@@ -0,0 +1,35 @@
+import ICommand from "../ICommand";
+import CommandMessage from "../CommandMessage";
+import Bot from "../Bot";
+
+class SummonCommand implements ICommand {
+    aliases: string[] = ["summon"];
+
+    execute(command: CommandMessage): void {
+        if (!command.arguments[0]) {
+            command.message.reply("Consider yourself summoned.");
+        }
+
+        const user = command.arguments[0].mention;
+
+        if (!user) {
+            command.message.reply("I'm not sure who that is.");
+        }
+
+        switch (user?.id) {
+            case Bot.client.user?.id:
+                command.message.channel.send("Do not worry, I am here.");
+                break;
+            case command.message.author.id:
+                command.message.reply("Consider yourself summoned.");
+                break;
+            default:
+                command.message.channel.send(`${user}, ${command.message.author} would like you to join them in their adventure.`);
+                break;
+        }
+
+        command.message.delete();
+    }
+}
+
+export = SummonCommand;
