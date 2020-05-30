147d9febfd40883b43ce3e630b6c9e5b7f4cfeb8
Toby <toby@tobymeehan.com>
2020-05-30 12:32:57 +0100

    Update bot.ts
    Changed configuration to include debug mode

--- Bot.ts
+++ Bot.ts
@@ -1,12 +1,26 @@
 import { Client } from "discord.js";
 import Config from "./Configuration.json";
+import DebugConfig from "./Configuration.Debug.json";
+import Auth from "./Authentication.json";
 
 /**
  * Represents the current bot.
  */
 class Bot {
-    static client: Client;
-    static configuration = Config;
+    static debugMode = false;
+    static client: Client = new Client();
+
+    static get configuration() {
+        return Bot.debugMode ? DebugConfig : Config;
+    }
+
+    static get token(): string {
+        return Bot.debugMode ? Auth.debugtoken : Auth.token;
+    }
+
+    static login() {
+        Bot.client.login(Bot.token);
+    }
 }
 
 export = Bot;
