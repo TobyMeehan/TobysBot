49d6412d645b8e359aabdf5eecd0aa26ae01a6b9
Toby <toby@tobymeehan.com>
2021-04-18 14:11:27 +0100

    Update rule34 command
    Added check for queries with no results

--- Commands/Rule34Command.ts
+++ Commands/Rule34Command.ts
@@ -2,6 +2,7 @@ import ICommand from "../ICommand";
 import CommandMessage from "../CommandMessage";
 import { searchByText, getPost, IPost } from "api-rule34-xxx";
 import Random from "../Random";
+import { Message } from "discord.js";
 
 class Rule34Command implements ICommand {
     aliases: string[] = ["rule34"];
@@ -9,35 +10,60 @@ class Rule34Command implements ICommand {
     async execute(command: CommandMessage): Promise<void> {
         command.message.channel.startTyping();
 
-        const list = await searchByText(command.arguments[0].content);
-        let index = Math.floor(Random.next(0, list.length - 1));
-        let post: IPost;
+        const list = await searchByText(command.argument);
 
-        do {
-            post = await getPost(list[index].id);
-            index = Math.floor(Random.next(0, list.length - 1));
-        } while(!this.postIsImage(post));
+        if (list.length === 0) {
+            command.message.reply("No results for that query");;
+            command.message.channel.stopTyping();
+            return;
+        }
 
-        await command.message.channel.send(this.getEmbed(post));
+        for (let i = 0; i < 10; i++) { // try 10 times to find a suitable image
+            const index = Math.floor(Random.next(0, list.length - 1))
+            const post = await getPost(list[index].id);
+
+            if (this.postIsImage(post)) {
+                await command.message.channel.send(this.getEmbed(post, command.message));
+                command.message.channel.stopTyping();
+                return;
+            }
+        }
+
+        command.message.reply("No image results for that query");
 
         command.message.channel.stopTyping();
     }
 
-    getEmbed(post: IPost) {
+    getEmbed(post: IPost, message: Message) {
         const image = post.pages[0].imgURL[1];
 
         return {
             "embed": {
+                "color": 11199907,
+                
                 "author": {
                     "name": "rule34.xxx",
                     "url": "https://rule34.xxx",
                     "icon_url": "https://rule34.xxx/apple-touch-icon-precomposed.png"
                 },
 
-                "color": 11199907,
+                "title": "Original Post",
+                "url": `https://rule34.xxx/index.php?s=view&page=post&id=${post.id}`,
+
+                "fields": [
+                    {
+                        "name": "Tags",
+                        "value": post.tags.map(x => x.name).join(", ")
+                    }
+                ],
 
                 "image": {
                     "url": image
+                },
+
+                "footer": {
+                    "icon_url": message.author.displayAvatarURL(),
+                    "text": `Requested by ${message.author.username}#${message.author.discriminator}`
                 }
             }
         }
