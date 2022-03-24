f4557970bb9a9aab3d0abfc4dedd6228f0fb7df1
TobyMeehan <toby@tobymeehan.com>
2022-03-24 12:56:07 +0000

    Update music module
    Removed submodules and added help docs

--- TobysBot.Discord.Client/TextCommands/Modules/MusicModule.cs
+++ TobysBot.Discord.Client/TextCommands/Modules/MusicModule.cs
@@ -24,6 +24,8 @@ namespace TobysBot.Discord.Client.TextCommands.Modules
         private IEmote StopEmote => new Emoji("⏹");
         private IEmote ClearEmote => new Emoji("⏏");
         private IEmote FastForwardEmote => new Emoji("⏩");
+        private IEmote ShuffleEmote => new Emoji("🔀");
+        private IEmote SkipEmote => new Emoji("⏭");
         
         public MusicModule(IAudioNode node, IAudioSource source, ILyricsProvider lyrics) : base(node)
         {
@@ -55,6 +57,7 @@ namespace TobysBot.Discord.Client.TextCommands.Modules
         }
         
         // Player
+        
         [Command("play", RunMode = RunMode.Async)]
         [Alias("p")]
         [Summary("Add the track to the queue or resume playback.")]
@@ -228,139 +231,7 @@ namespace TobysBot.Discord.Client.TextCommands.Modules
                     .Build());
             }
         }
-
-        [Group("skip")]
-        public class SkipModule : VoiceModuleBase
-        {
-            
-            private IEmote SkipEmote => new Emoji("⏭");
-            
-            private readonly IAudioNode _node;
-
-            public SkipModule(IAudioNode node) : base(node)
-            {
-                _node = node;
-            }
-
-            [Command]
-            public async Task SkipAsync()
-            {
-                if (!await EnsureUserInSameVoiceAsync())
-                {
-                    return;
-                }
-
-                var track = await _node.SkipAsync(Context.Guild);
-
-                await Context.Message.AddReactionAsync(SkipEmote);
-            }
-
-            [Command("to")]
-            public async Task ToAsync(int index)
-            {
-                if (!await EnsureUserInSameVoiceAsync())
-                {
-                    return;
-                }
-
-                var queue = await _node.GetQueueAsync(Context.Guild);
-
-                if (index < 1 || index > queue.Count())
-                {
-                    await Context.Message.ReplyAsync(embed: new EmbedBuilder()
-                        .WithContext(EmbedContext.Error)
-                        .WithDescription("There is not a track at that position in the queue.")
-                        .Build());
-                    
-                    return;
-                }
-
-                var track = await _node.SkipAsync(Context.Guild, index);
-
-                await Context.Message.AddReactionAsync(SkipEmote);
-            }
-        }
         
-        [Group("shuffle")]
-        public class Shuffle : VoiceModuleBase
-        {
-            private readonly IAudioNode _node;
-
-            private IEmote ShuffleEmote => new Emoji("🔀");
-            
-            public Shuffle(IAudioNode node) : base(node)
-            {
-                _node = node;
-            }
-
-            [Command]
-            public async Task ShuffleAsync()
-            {
-                if (!await EnsureUserInSameVoiceAsync())
-                {
-                    return;
-                }
-
-                var queue = await _node.GetQueueAsync(Context.Guild);
-
-                switch (queue.ShuffleEnabled)
-                {
-                    case EnabledShuffleSetting:
-                        await ShuffleOffAsync();
-                        break;
-                        
-                    case DisabledShuffleSetting:
-                        await ShuffleOnAsync();
-                        break;
-                }
-            }
-
-            [Command("on")]
-            public async Task ShuffleOnAsync()
-            {
-                if (!await EnsureUserInSameVoiceAsync())
-                {
-                    return;
-                }
-
-                await _node.SetShuffleAsync(Context.Guild, new EnabledShuffleSetting());
-
-                await Context.Message.ReplyAsync(embed: new EmbedBuilder()
-                    .WithContext(EmbedContext.Action)
-                    .WithDescription("Shuffle mode is **enabled**.")
-                    .Build());
-            }
-
-            [Command("off")]
-            public async Task ShuffleOffAsync()
-            {
-                if (!await EnsureUserInSameVoiceAsync())
-                {
-                    return;
-                }
-
-                await _node.SetShuffleAsync(Context.Guild, new DisabledShuffleSetting());
-
-                await Context.Message.ReplyAsync(embed: new EmbedBuilder()
-                    .WithContext(EmbedContext.Action)
-                    .WithDescription("Shuffle mode is **disabled**.")
-                    .Build());
-            }
-
-            [Command("once")]
-            public async Task ShuffleOnceAsync()
-            {
-                if (!await EnsureUserInSameVoiceAsync())
-                {
-                    return;
-                }
-
-                await _node.ShuffleAsync(Context.Guild);
-
-                await Context.Message.AddReactionAsync(ShuffleEmote);
-            }
-        }
-
         [Command("stop")]
         [Summary("Stop playback and return to the start of the queue.")]
         public async Task StopAsync()
@@ -373,6 +244,48 @@ namespace TobysBot.Discord.Client.TextCommands.Modules
             await _node.StopAsync(Context.Guild);
             
             await Context.Message.AddReactionAsync(StopEmote);
+        }
+        
+        // Queue
+        
+        [Command("skip")]
+        [Summary("Skip to the next track.")]
+        public async Task SkipAsync()
+        {
+            if (!await EnsureUserInSameVoiceAsync())
+            {
+                return;
+            }
+
+            var track = await _node.SkipAsync(Context.Guild);
+
+            await Context.Message.AddReactionAsync(SkipEmote);
+        }
+
+        [Command("skip to")]
+        [Summary("Skip to the specified track.")]
+        public async Task SkipToAsync(int track)
+        {
+            if (!await EnsureUserInSameVoiceAsync())
+            {
+                return;
+            }
+
+            var queue = await _node.GetQueueAsync(Context.Guild);
+
+            if (track < 1 || track > queue.Count())
+            {
+                await Context.Message.ReplyAsync(embed: new EmbedBuilder()
+                    .WithContext(EmbedContext.Error)
+                    .WithDescription("There is not a track at that position in the queue.")
+                    .Build());
+                
+                return;
+            }
+
+            await _node.SkipAsync(Context.Guild, track);
+
+            await Context.Message.AddReactionAsync(SkipEmote);
         }
 
         [Command("clear")]
@@ -389,104 +302,190 @@ namespace TobysBot.Discord.Client.TextCommands.Modules
             await Context.Message.AddReactionAsync(ClearEmote);
         }
         
-        [Group("loop")]
-        public class LoopModule : VoiceModuleBase
+        [Command("loop")]
+        [HideInHelp]
+        public async Task LoopAsync()
         {
-            private readonly IAudioNode _node;
-
-            public LoopModule(IAudioNode node) : base(node)
+            if (!await EnsureUserInSameVoiceAsync())
             {
-                _node = node;
+                return;
+            }
+
+            var queue = await _node.GetQueueAsync(Context.Guild);
+            
+            if (queue is null)
+            {
+                await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildNotPlayingEmbed());
+                return;
+            }
+
+            if (queue.LoopEnabled is EnabledLoopSetting)
+            {
+                await _node.SetLoopAsync(Context.Guild, new DisabledLoopSetting());
+                await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildLoopDisabledEmbed());
+                return;
             }
             
-            [Command]
-            public async Task LoopAsync()
+            if (queue.CurrentTrack is not null && !queue.Next().Any())
             {
-                if (!await EnsureUserInSameVoiceAsync())
-                {
-                    return;
-                }
-
-                var queue = await _node.GetQueueAsync(Context.Guild);
-                
-                if (queue is null)
-                {
-                    await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildNotPlayingEmbed());
-                    return;
-                }
-
-                if (queue.LoopEnabled is EnabledLoopSetting)
-                {
-                    await _node.SetLoopAsync(Context.Guild, new DisabledLoopSetting());
-                    await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildLoopDisabledEmbed());
-                    return;
-                }
-                
-                if (queue.CurrentTrack is not null && !queue.Next().Any())
-                {
-                    await _node.SetLoopAsync(Context.Guild, new TrackLoopSetting());
-                    await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildLoopTrackEmbed());
-                    return;
-                }
-                
-                await _node.SetLoopAsync(Context.Guild, new QueueLoopSetting());
-                await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildLoopQueueEmbed());
-            }
-
-            [Command("track")]
-            public async Task TrackAsync()
-            {
-                if (!await EnsureUserInSameVoiceAsync())
-                {
-                    return;
-                }
-
-                var queue = await _node.GetQueueAsync(Context.Guild);
-                
-                if (queue is null)
-                {
-                    await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildNotPlayingEmbed());
-                    return;
-                }
-
-                if (queue.LoopEnabled is TrackLoopSetting)
-                {
-                    await _node.SetLoopAsync(Context.Guild, new DisabledLoopSetting());
-                    await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildLoopDisabledEmbed());
-                    return;
-                }
-
                 await _node.SetLoopAsync(Context.Guild, new TrackLoopSetting());
                 await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildLoopTrackEmbed());
+                return;
+            }
+            
+            await _node.SetLoopAsync(Context.Guild, new QueueLoopSetting());
+            await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildLoopQueueEmbed());
+        }
+
+        [Command("loop track")]
+        [Summary("Loop the current track.")]
+        public async Task LoopTrackAsync()
+        {
+            if (!await EnsureUserInSameVoiceAsync())
+            {
+                return;
             }
 
-            [Command("queue")]
-            public async Task QueueAsync()
+            var queue = await _node.GetQueueAsync(Context.Guild);
+            
+            if (queue is null)
             {
-                if (!await EnsureUserInSameVoiceAsync())
-                {
-                    return;
-                }
+                await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildNotPlayingEmbed());
+                return;
+            }
 
-                var queue = await _node.GetQueueAsync(Context.Guild);
-                
-                if (queue is null)
-                {
-                    await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildNotPlayingEmbed());
-                    return;
-                }
+            if (queue.LoopEnabled is TrackLoopSetting)
+            {
+                await _node.SetLoopAsync(Context.Guild, new DisabledLoopSetting());
+                await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildLoopDisabledEmbed());
+                return;
+            }
 
-                if (queue.LoopEnabled is QueueLoopSetting)
-                {
-                    await _node.SetLoopAsync(Context.Guild, new DisabledLoopSetting());
-                    await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildLoopDisabledEmbed());
-                    return;
-                }
+            await _node.SetLoopAsync(Context.Guild, new TrackLoopSetting());
+            await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildLoopTrackEmbed());
+        }
 
-                await _node.SetLoopAsync(Context.Guild, new QueueLoopSetting());
-                await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildLoopQueueEmbed());
+        [Command("loop queue")]
+        [Summary("Loop the queue.")]
+        public async Task LoopQueueAsync()
+        {
+            if (!await EnsureUserInSameVoiceAsync())
+            {
+                return;
+            }
+
+            var queue = await _node.GetQueueAsync(Context.Guild);
+            
+            if (queue is null)
+            {
+                await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildNotPlayingEmbed());
+                return;
+            }
+
+            if (queue.LoopEnabled is QueueLoopSetting)
+            {
+                await _node.SetLoopAsync(Context.Guild, new DisabledLoopSetting());
+                await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildLoopDisabledEmbed());
+                return;
+            }
+
+            await _node.SetLoopAsync(Context.Guild, new QueueLoopSetting());
+            await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildLoopQueueEmbed());
+        }
+
+        [Command("loop off")]
+        [Summary("Disable looping.")]
+        public async Task LoopOffAsync()
+        {
+            if (!await EnsureUserInSameVoiceAsync())
+            {
+                return;
+            }
+
+            var queue = await _node.GetQueueAsync(Context.Guild);
+
+            if (queue is null)
+            {
+                await Context.Message.ReplyAsync(embed: new EmbedBuilder().BuildNotPlayingEmbed());
+                return;
+            }
+
+            await _node.SetLoopAsync(Context.Guild, new DisabledLoopSetting());
+            await Context.Message.AddReactionAsync(StopEmote);
+        }
+
+        [Command("shuffle")]
+        [Summary("Toggle shuffle mode.")]
+        public async Task ShuffleAsync()
+        {
+            if (!await EnsureUserInSameVoiceAsync())
+            {
+                return;
+            }
+
+            var queue = await _node.GetQueueAsync(Context.Guild);
+
+            switch (queue.ShuffleEnabled)
+            {
+                case EnabledShuffleSetting:
+                    await ShuffleOffAsync();
+                    break;
+                    
+                case DisabledShuffleSetting:
+                    await ShuffleOnAsync();
+                    break;
             }
         }
+
+        [Command("shuffle on")]
+        [HideInHelp]
+        public async Task ShuffleOnAsync()
+        {
+            if (!await EnsureUserInSameVoiceAsync())
+            {
+                return;
+            }
+
+            await _node.SetShuffleAsync(Context.Guild, new EnabledShuffleSetting());
+
+            await Context.Message.ReplyAsync(embed: new EmbedBuilder()
+                .WithContext(EmbedContext.Action)
+                .WithDescription("Shuffle mode is **enabled**.")
+                .Build());
+        }
+
+        [Command("shuffle off")]
+        [HideInHelp]
+        public async Task ShuffleOffAsync()
+        {
+            if (!await EnsureUserInSameVoiceAsync())
+            {
+                return;
+            }
+
+            await _node.SetShuffleAsync(Context.Guild, new DisabledShuffleSetting());
+
+            await Context.Message.ReplyAsync(embed: new EmbedBuilder()
+                .WithContext(EmbedContext.Action)
+                .WithDescription("Shuffle mode is **disabled**.")
+                .Build());
+        }
+
+        [Command("shuffle once")]
+        [Summary("Shuffle the queue.")]
+        public async Task ShuffleOnceAsync()
+        {
+            if (!await EnsureUserInSameVoiceAsync())
+            {
+                return;
+            }
+
+            await _node.ShuffleAsync(Context.Guild);
+
+            await Context.Message.AddReactionAsync(ShuffleEmote);
+        }
+        
+        // Information
 
         [Command("np")]
         [Summary("Display the currently playing track.")]
