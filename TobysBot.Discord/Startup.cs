1cb7af1ad0615255c8adb70809b10d2646eb0222
TobyMeehan <toby@tobymeehan.com>
2022-03-24 13:13:27 +0000

    Add activity
    Added activity showing current prefix

--- TobysBot.Discord/Startup.cs
+++ TobysBot.Discord/Startup.cs
@@ -32,6 +32,7 @@ namespace TobysBot.Discord
             services.AddDiscordClient(options =>
                 {
                     options.Token = Configuration.GetSection("Discord")["Token"];
+                    options.Prefix = Configuration.GetSection("Discord")["Prefix"];
                 })
                 .AddLavaNode(options =>
                 {
