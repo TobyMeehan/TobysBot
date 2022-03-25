73561b8bd0164b19fb3e97a26efd97f8f3fbedab
TobyMeehan <toby@tobymeehan.com>
2022-03-25 16:49:40 +0000

    Add summon command

--- TobysBot.Discord.Client/TextCommands/Modules/ClassicModule.cs
+++ TobysBot.Discord.Client/TextCommands/Modules/ClassicModule.cs
@@ -25,7 +25,7 @@ public class ClassicModule : ModuleBase<SocketCommandContext>
         _httpClientFactory = httpClientFactory;
         _options = options.Value;
     }
-    
+
     [Command("pop")]
     [Summary("Calls the user a pop pop head.")]
     public async Task PopAsync(IUser user = null)
@@ -41,7 +41,7 @@ public class ClassicModule : ModuleBase<SocketCommandContext>
             await Context.Message.ReplyAsync("I am not a pop pop head, how dare you!");
             return;
         }
-        
+
         if (user.Id == _options.TobyId)
         {
             await ReplyAsync($"{user.Mention} is a star. S T A R  S T A R");
@@ -49,6 +49,55 @@ public class ClassicModule : ModuleBase<SocketCommandContext>
         else
         {
             await ReplyAsync($"{user.Mention} is a pop pop head.");
+        }
+
+        await Context.Message.DeleteAsync();
+    }
+
+    [Command("summon")]
+    [Summary("Summons the user to your activity.")]
+    public async Task SummonAsync(IUser user)
+    {
+        if (user.Id == Context.Client.CurrentUser.Id)
+        {
+            await Context.Message.ReplyAsync("Do not worry, I am here.");
+            return;
+        }
+
+        if (user.Id == Context.User.Id)
+        {
+            await Context.Message.ReplyAsync("Consider yourself summoned.");
+            return;
+        }
+
+        if (Context.User.Activities.Any())
+        {
+            var activity = Context.User.Activities.First();
+
+            await ReplyAsync($"{user.Mention}, {Context.User.Mention} wants you to join them in {activity.Name}");
+        }
+        else if (Context.User is IVoiceState voiceState && voiceState.VoiceChannel?.GuildId == Context.Guild.Id)
+        {
+            if (voiceState.IsStreaming)
+            {
+                await ReplyAsync(
+                    $"{user.Mention}, {Context.User.Mention} wants you to watch their stream in {voiceState.VoiceChannel.Mention}.");
+            }
+            else
+            {
+                await ReplyAsync(
+                    $"{user.Mention}, {Context.User.Mention} wants you to join them in {voiceState.VoiceChannel.Mention}");
+            }
+
+            if (voiceState.IsSelfDeafened || voiceState.IsSelfMuted)
+            {
+                await ReplyAsync($"{user.Mention} you should probably unmute though...");
+            }
+        }
+        else
+        {
+            await ReplyAsync(
+                $"{user.Mention}, {Context.User.Mention} wants you to join them in whatever they are doing.");
         }
 
         await Context.Message.DeleteAsync();
