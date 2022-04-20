fd2e4a3caaa97c403ab4a3f841d2c0f85983298a
TobyMeehan <toby@tobymeehan.com>
2022-04-20 14:13:36 +0100

    Add spotify to audio source
    Added support for spotify links to audio source

--- TobysBot.Discord.Audio/Lavalink/LavalinkAudioSource.cs
+++ TobysBot.Discord.Audio/Lavalink/LavalinkAudioSource.cs
@@ -6,7 +6,9 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using Discord;
+using SpotifyAPI.Web;
 using TobysBot.Discord.Audio.Extensions;
+using TobysBot.Discord.Audio.Spotify;
 using Victoria;
 using Victoria.Responses.Search;
 
@@ -16,11 +18,13 @@ namespace TobysBot.Discord.Audio.Lavalink
     {
         private readonly LavaNode<XLavaPlayer> _node;
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ISpotifyClient _spotify;
 
-        public LavalinkAudioSource(LavaNode<XLavaPlayer> node, IHttpClientFactory httpClientFactory)
+        public LavalinkAudioSource(LavaNode<XLavaPlayer> node, IHttpClientFactory httpClientFactory, ISpotifyClient spotify)
         {
             _node = node;
             _httpClientFactory = httpClientFactory;
+            _spotify = spotify;
         }
         
         public async Task<IPlayable> SearchAsync(string query)
@@ -33,6 +37,10 @@ namespace TobysBot.Discord.Audio.Lavalink
                     case "www.youtube.com":
                     case "music.youtube.com":
                         return await LoadFromYoutubeAsync(uri);
+                    
+                    case "open.spotify.com":
+
+                        return await LoadFromSpotifyAsync(uri);
                     
                     default:
                         return await LoadFromUriAsync(uri);
@@ -51,6 +59,81 @@ namespace TobysBot.Discord.Audio.Lavalink
                 SearchStatus.LoadFailed => new NotPlayable(new Exception(result.Exception.Message)),
                 _ => throw new ArgumentOutOfRangeException()
             };
+        }
+
+        private async Task<IPlayable> LoadFromSpotifyAsync(Uri uri)
+        {
+            return uri.Segments[1] switch
+            {
+                "track" => await LoadSpotifyTrackAsync(uri.Segments[2]),
+                "playlist" => await LoadSpotifyPlaylistAsync(uri.Segments[2]),
+                _ => new NotPlayable(new Exception("Invalid spotify url."))
+            };
+        }
+
+        private async Task<IPlayable> LoadSpotifyTrackAsync(string id)
+        {
+            var track = await _spotify.Tracks.Get(id);
+            
+            var query = $"{track.Artists[0].Name} {track.Name}";
+            
+            var search = await _node.SearchYouTubeAsync(query);
+
+            if (search.Status is not SearchStatus.SearchResult)
+            {
+                return new NotPlayable(new Exception("No results for spotify track."));
+            }
+
+            foreach (var ytTrack in search.Tracks)
+            {
+                var uri = new Uri(ytTrack.Url);
+                var playable = await LoadFromYoutubeAsync(new Uri($"{uri.Scheme}music.youtube.com{uri.PathAndQuery}"));
+
+                if (playable is not ITrack result)
+                {
+                    continue;
+                }
+
+                return new SpotifyTrack(track, result);
+            }
+
+            return new NotPlayable(new Exception("Could not find source for spotify track."));
+        }
+
+        private async Task<IPlayable> LoadSpotifyPlaylistAsync(string id)
+        {
+            var playlist = await _spotify.Playlists.Get(id);
+
+            if (playlist.Tracks?.Items is null)
+            {
+                return new NotPlayable(new Exception("Could not load spotify playlist."));
+            }
+            
+            var tracks = new List<SpotifyTrack>();
+
+            foreach (var track in playlist.Tracks.Items)
+            {
+                if (track.Track.Type is ItemType.Episode)
+                {
+                    return new NotPlayable(new Exception("Podcasts are not currently supported."));
+                }
+
+                var playable = await LoadSpotifyTrackAsync((track.Track as FullTrack)?.Id);
+
+                if (playable is not SpotifyTrack spotifyTrack)
+                {
+                    continue;
+                }
+                
+                tracks.Add(spotifyTrack);
+            }
+
+            if (!tracks.Any())
+            {
+                return new NotPlayable(new Exception("Could not load spotify playlist."));
+            }
+
+            return new SpotifyPlaylist(playlist, tracks);
         }
 
         private async Task<IPlayable> LoadFromYoutubeAsync(Uri uri)
---
+++ TobysBot.Discord.Audio/Spotify/SpotifyPlaylist.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using SpotifyAPI.Web;
+
+namespace TobysBot.Discord.Audio.Spotify;
+
+public class SpotifyPlaylist : IPlaylist
+{
+    private readonly IEnumerable<SpotifyTrack> _tracks;
+
+    public SpotifyPlaylist(FullPlaylist playlist, IEnumerable<SpotifyTrack> tracks)
+    {
+        _tracks = tracks;
+        Url = playlist.Uri;
+        Title = playlist.Name;
+    }
+    
+    public string Url { get; }
+    public string Title { get; }
+    
+    public IEnumerator<ITrack> GetEnumerator()
+    {
+        return _tracks.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
