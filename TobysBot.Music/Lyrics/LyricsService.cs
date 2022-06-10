68c7a72af1a280212b8d50960b63b203f0c67e10
Toby <toby@tobymeehan.com>
2022-06-10 16:31:54 +0100

    Add lyrics service

---
+++ TobysBot.Music/Lyrics/LyricsService.cs
@@ -0,0 +1,26 @@
+﻿namespace TobysBot.Music.Lyrics;
+
+public class LyricsService : ILyricsService
+{
+    private readonly IEnumerable<ILyricsResolver> _resolvers;
+
+    public LyricsService(IEnumerable<ILyricsResolver> resolvers)
+    {
+        _resolvers = resolvers.OrderByDescending(x => x.Priority);
+    }
+    
+    public async Task<ILyricsResult> GetLyricsAsync(ITrack track)
+    {
+        foreach (var resolver in _resolvers)
+        {
+            var result = await resolver.TryResolveAsync(track);
+
+            if (result.Success)
+            {
+                return result;
+            }
+        }
+
+        return new LyricsResult();
+    }
+}
