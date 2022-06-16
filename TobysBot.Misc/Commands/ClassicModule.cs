ecc3ff82fa2d215ae7f8a11f71d9391a7a852f57
Toby <toby@tobymeehan.com>
2022-06-17 00:36:38 +0100

    Add summon command

--- TobysBot.Misc/Commands/ClassicModule.cs
+++ TobysBot.Misc/Commands/ClassicModule.cs
@@ -12,12 +12,12 @@ namespace TobysBot.Misc.Commands;
 
 public class ClassicModule : CommandModuleBase
 {
-    private readonly EmbedService _embeds;
     private readonly StarOptions _options;
 
-    public ClassicModule(EmbedService embeds, IOptions<StarOptions> options)
+    private IEmote OkEmote => new Emoji("👌");
+    
+    public ClassicModule(IOptions<StarOptions> options)
     {
-        _embeds = embeds;
         _options = options.Value;
     }
     
@@ -42,10 +42,7 @@ public class ClassicModule : CommandModuleBase
             return;
         }
 
-        await Response.ReplyAsync(visibility: Visibility.Hidden, embed: _embeds.Builder()
-            .WithContext(EmbedContext.Action)
-            .WithDescription("Popping!")
-            .Build());
+        await Response.ReactAsync(OkEmote, Visibility.Ephemeral);
         
         if (_options.StarUsers.Contains(user.Id))
         {
@@ -68,10 +65,7 @@ public class ClassicModule : CommandModuleBase
         [Summary("Star to declare.")] 
         IUser user = null)
     {
-        await Response.ReplyAsync(visibility: Visibility.Hidden, embed: _embeds.Builder()
-            .WithContext(EmbedContext.Action)
-            .WithDescription("Starring!")
-            .Build());
+        await Response.ReactAsync(OkEmote, Visibility.Ephemeral);
         
         if (user is null)
         {
@@ -95,6 +89,71 @@ public class ClassicModule : CommandModuleBase
         }
 
         await Context.Channel.SendMessageAsync(Star(user));
+
+        if (Context.Message is not null)
+        {
+            await Context.Message.DeleteAsync();
+        }
+    }
+
+    [Command("summon")]
+    [Summary("Summons the user to your activity.")]
+    public async Task SummonAsync(
+        [Summary("User to summon.")]
+        IUser user = null, 
+        [Summary("Role to summon.")]
+        IRole role = null)
+    {
+        IMentionable mention = role;
+        user ??= Context.Client.CurrentUser;
+        mention ??= user;
+
+        if (mention is IUser)
+        {
+            if (user.Id == Context.Client.CurrentUser.Id)
+            {
+                await Response.ReplyAsync("Do not worry, I am here.");
+                return;
+            }
+
+            if (user.Id == Context.User.Id)
+            {
+                await Context.Message.ReplyAsync("Consider yourself summoned.");
+                return;
+            }
+        }
+
+        await Response.ReactAsync(OkEmote, Visibility.Ephemeral);
+        
+        if (Context.User.Activities.Any())
+        {
+            var activity = Context.User.Activities.First();
+
+            await ReplyAsync($"{mention.Mention}, {Context.User.Mention} wants you to join the in {activity.Name}");
+        }
+        else if (Context.User is IVoiceState voiceState && voiceState.VoiceChannel?.GuildId == Context.Guild.Id)
+        {
+            if (voiceState.IsStreaming)
+            {
+                await ReplyAsync(
+                    $"{mention.Mention}, {Context.User.Mention} wants you to watch their stream in {voiceState.VoiceChannel.Mention}");
+            }
+            else
+            {
+                await ReplyAsync(
+                    $"{mention.Mention}, {Context.User.Mention} wants you to join them in {voiceState.VoiceChannel.Mention}");
+            }
+
+            if (voiceState.IsSelfDeafened || voiceState.IsSelfMuted)
+            {
+                await ReplyAsync($"{Context.User.Mention} you should probably unmute yourself though...");
+            }
+        }
+        else
+        {
+            await ReplyAsync(
+                $"{mention.Mention}, {Context.User.Mention} wants you to join them in whatever they are doing.");
+        }
 
         if (Context.Message is not null)
         {
