f9057e9c066757e42f655e5f4bf508ed838d4cd8
Toby <toby@tobymeehan.com>
2022-06-17 19:23:51 +0100

    Add music error messages
    Added missing error messages for music plugin

--- TobysBot.Music/Configuration/MusicOptions.cs
+++ TobysBot.Music/Configuration/MusicOptions.cs
@@ -11,16 +11,16 @@ public class MusicOptions
 
 public class MusicEmbedOptions
 {
-    public string NotFoundErrorDescription { get; set; }
-    public string NotPlayingErrorDescription { get; set; }
-    public string AlreadyPlayingErrorDescription { get; set; }
-    public string AlreadyPausedErrorDescription { get; set; }
-    public string CannotParseTimestampErrorDescription { get; set; }
-    public string TimestampTooLongErrorDescription { get; set; }
-    public string FastForwardTooFarErrorDescription { get; set; }
-    public string RewindTooFarErrorDescription { get; set; }
-    public string NoPreviousTrackErrorDescription { get; set; }
-    public string PositionOutOfRangeErrorDescription { get; set; }
+    public string NotFoundErrorDescription { get; set; } = "No results for that query.";
+    public string NotPlayingErrorDescription { get; set; } = "I'm not playing anything.";
+    public string AlreadyPlayingErrorDescription { get; set; } = "The track is already playing!";
+    public string AlreadyPausedErrorDescription { get; set; } = "The track is already paused!";
+    public string CannotParseTimestampErrorDescription { get; set; } = "Could not parse that timestamp.";
+    public string TimestampTooLongErrorDescription { get; set; } = "The track is not that long.";
+    public string FastForwardTooFarErrorDescription { get; set; } = "Can't fast forward beyond the track!";
+    public string RewindTooFarErrorDescription { get; set; } = "Can't rewind to before the track!";
+    public string NoPreviousTrackErrorDescription { get; set; } = "No previous track to jump to.";
+    public string PositionOutOfRangeErrorDescription { get; set; } = "No track at that position";
 }
 
 public class SpotifyOptions
