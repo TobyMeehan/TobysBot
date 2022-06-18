04f6b15cd6856fe3cdf4c6d0fa5535fa70b44d50
Toby <toby@tobymeehan.com>
2022-06-18 20:42:06 +0100

    Add mongodb data access
    Added data access implementation for mongodb

---
+++ TobysBot.Mongo/Data/MongoDataAccess.cs
@@ -0,0 +1,78 @@
+﻿using MongoDB.Driver;
+using TobysBot.Data;
+using TobysBot.Mongo.Client;
+
+namespace TobysBot.Mongo.Data;
+
+public class MongoDataAccess : IDataAccess
+{
+    private readonly IMongoService _service;
+
+    public MongoDataAccess(IMongoService service)
+    {
+        _service = service;
+    }
+
+    public async Task<T> SaveAsync<T>(string collectionName, T data) where T : IEntity
+    {
+        var collection = _service.Connect<T>(collectionName);
+
+        await collection.InsertOneAsync(data);
+
+        return data;
+    }
+
+    public async Task<IReadOnlyCollection<T>> GetAsync<T>(string collectionName) where T : IEntity
+    {
+        var collection = _service.Connect<T>(collectionName);
+
+        var result = await collection.FindAsync(_ => true);
+
+        return result.ToList();
+    }
+
+    public async Task<T> GetAsync<T>(string collectionName, string id) where T : IEntity
+    {
+        var collection = _service.Connect<T>(collectionName);
+
+        var result = await collection.FindAsync(x => x.Id == id);
+
+        return result.FirstOrDefault();
+    }
+
+    public async Task<T> GetAsync<T>(string collectionName, ulong id) where T : IDiscordEntity
+    {
+        var collection = _service.Connect<T>(collectionName);
+
+        var result = await collection.FindAsync(x => x.DiscordId == id);
+
+        return result.FirstOrDefault();
+    }
+
+    public async Task<T> GetByNameAsync<T>(string collectionName, string name) where T : INamedEntity
+    {
+        var collection = _service.Connect<T>(collectionName);
+
+        var result = await collection.FindAsync(x => x.Name == null);
+
+        return result.FirstOrDefault();
+    }
+
+    public async Task<T> UpdateAsync<T>(string collectionName, T data) where T : IEntity
+    {
+        var collection = _service.Connect<T>(collectionName);
+
+        var filter = Builders<T>.Filter.Eq(x => x.Id, data.Id);
+
+        await collection.ReplaceOneAsync(filter, data, new ReplaceOptions {IsUpsert = false});
+
+        return data;
+    }
+
+    public async Task DeleteAsync<T>(string collectionName, T data) where T : IEntity
+    {
+        var collection = _service.Connect<T>(collectionName);
+
+        await collection.DeleteOneAsync(x => x.Id == data.Id);
+    }
+}
--- TobysBot/Data/IDataAccess.cs
+++ TobysBot/Data/IDataAccess.cs
@@ -2,17 +2,17 @@
 
 public interface IDataAccess
 {
-    Task<T> SaveAsync<T>(T data) where T : IEntity;
+    Task<T> SaveAsync<T>(string collectionName, T data) where T : IEntity;
 
-    Task<IReadOnlyCollection<T>> GetAsync<T>() where T : IEntity;
+    Task<IReadOnlyCollection<T>> GetAsync<T>(string collectionName) where T : IEntity;
 
-    Task<T> GetAsync<T>(string id) where T : IEntity;
+    Task<T> GetAsync<T>(string collectionName, string id) where T : IEntity;
 
-    Task<T> GetAsync<T>(ulong id) where T : IDiscordEntity;
+    Task<T> GetAsync<T>(string collectionName, ulong id) where T : IDiscordEntity;
 
-    Task<T> GetByNameAsync<T>(string name) where T : INamedEntity;
+    Task<T> GetByNameAsync<T>(string collectionName, string name) where T : INamedEntity;
 
-    Task<T> UpdateAsync<T>(T data) where T : IEntity;
+    Task<T> UpdateAsync<T>(string collectionName, T data) where T : IEntity;
 
-    Task DeleteAsync<T>(T data) where T : IEntity;
+    Task DeleteAsync<T>(string collectionName, T data) where T : IEntity;
 }
