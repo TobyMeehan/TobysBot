9777429050ff5e08ff9b5f229f1e01f252b49fb7
Toby <toby@tobymeehan.com>
2022-06-22 01:59:35 +0100

    Update vaporwave preset
    Increased speed and pitch and added high and low pass equalizer

--- src/TobysBot.Voice/Effects/VaporwavePreset.cs
+++ src/TobysBot.Voice/Effects/VaporwavePreset.cs
@@ -2,6 +2,8 @@
 
 public class VaporwavePreset : BasePreset
 {
-    public override double Speed => 0.8;
-    public override double Pitch => 0.8;
+    public override double Speed => 0.83;
+    public override double Pitch => 0.875;
+
+    public override IEqualizer Equalizer => new VaporwaveEqualizer();
 }
