8fe99e224109347aef44b7e97048ea85ae539d30
Toby <toby@tobymeehan.com>
2022-06-22 21:26:18 +0100

    Tweak vaporwave pitch

--- src/TobysBot.Voice/Effects/VaporwavePreset.cs
+++ src/TobysBot.Voice/Effects/VaporwavePreset.cs
@@ -3,7 +3,7 @@
 public class VaporwavePreset : BasePreset
 {
     public override double Speed => 0.83;
-    public override double Pitch => 0.875;
+    public override double Pitch => 0.89;
 
     public override IEqualizer Equalizer => new VaporwaveEqualizer();
 }
