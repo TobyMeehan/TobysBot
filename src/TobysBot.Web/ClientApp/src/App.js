10ef74e6f0b7d02915d61688ef18496437f836bf
Toby <toby@tobymeehan.com>
2022-06-25 06:11:23 +0100

    Add web icons
    Added favicons for website

--- src/TobysBot.Web/ClientApp/src/App.js
+++ src/TobysBot.Web/ClientApp/src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './shared/Layout';
-import { Home } from './pages/Home';
-import { Commands } from "./pages/Commands";
-import { FetchData } from './components/FetchData';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import React, {Component} from 'react';
+import {Route} from 'react-router';
+import {Layout} from './shared/Layout';
+import {Home} from './pages/Home';
+import {Commands} from "./pages/Commands";
+import {FetchData} from './components/FetchData';
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {fab} from '@fortawesome/free-brands-svg-icons'
+import {fas} from '@fortawesome/free-solid-svg-icons'
 
 import './custom.css'
+import {Helmet} from "react-helmet";
 
 
 library.add(fab, fas)
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/commands' component={Commands} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <>
+                <Helmet>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                    <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                    <link rel="manifest" href="/site.webmanifest"/>
+                    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#32072c"/>
+                    <meta name="apple-mobile-web-app-title" content="Toby's Bot"/>
+                    <meta name="application-name" content="Toby's Bot"/>
+                    <meta name="msapplication-TileColor" content="#32072c"/>
+                    <meta name="theme-color" content="#32072c"/>
+                </Helmet>
+
+                <Layout>
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/commands' component={Commands}/>
+                    <Route path='/fetch-data' component={FetchData}/>
+                </Layout>
+            </>
+        );
+    }
 }
