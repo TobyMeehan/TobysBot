4ec00266f5733c68cd96d4776555958082bb5fe9
Toby <toby@tobymeehan.com>
2022-06-25 06:21:44 +0100

    Remove weather forecast
    Removed template components from website

--- src/TobysBot.Web/ClientApp/src/App.js
+++ src/TobysBot.Web/ClientApp/src/App.js
@@ -3,7 +3,6 @@ import {Route} from 'react-router';
 import {Layout} from './shared/Layout';
 import {Home} from './pages/Home';
 import {Commands} from "./pages/Commands";
-import {FetchData} from './components/FetchData';
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {fab} from '@fortawesome/free-brands-svg-icons'
 import {fas} from '@fortawesome/free-solid-svg-icons'
@@ -35,7 +34,6 @@ export default class App extends Component {
                 <Layout>
                     <Route exact path='/' component={Home}/>
                     <Route path='/commands' component={Commands}/>
-                    <Route path='/fetch-data' component={FetchData}/>
                 </Layout>
             </>
         );
