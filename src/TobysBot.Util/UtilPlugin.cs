209267abd1e2452689f2c7554e716bccbbd4e31e
Toby <toby@tobymeehan.com>
2022-07-05 13:06:58 +0100

    Add util plugin
    Added project for utilities command plugin

---
+++ src/TobysBot.Util/UtilPlugin.cs
@@ -0,0 +1,8 @@
+﻿namespace TobysBot.Util;
+
+public class UtilPlugin : IPluginRegistration
+{
+    public string Id => "util";
+    public string Name => "Util";
+    public string Description => "Various useful utilities.";
+}
